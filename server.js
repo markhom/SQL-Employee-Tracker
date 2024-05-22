7818d490e3e81fedd8a76d8812d9a0bec6ec8f78
Mark Homchik <markhomchik4@gmail.com>
2024-05-21 20:47:50 -0500

    continuing to add functions

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 import inquirer from 'inquirer';
 // Import and require mysql2
 import mysql from 'mysql2'
-
-const PORT = process.env.PORT || 3001;
-// const app = express();
+import { table } from 'table';
 
 // Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        // MySQL username,
+        port: 3001,
         user: 'root',
         password: '',
         database: 'employee_tracker_db'
@@ -69,7 +67,7 @@ function employeeTracker() {
 
 //Choice functions
 
-function viewDepartment() {
+function addDepartment() {
     inquirer.prompt({
         type: "input",
         messsage: "What is the name of the department?",
@@ -81,6 +79,66 @@ function viewDepartment() {
             employeeTracker();
         }
     })
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What's the name of the role?",
+            name: "roleName"
+        },
+        {
+            type: "input",
+            message: "What is the salary for this role?",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "What is the department id number?",
+            name: "departmentId"
+        }
+    ])
+    .then(function(answer) {
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.roleName, answer.salary, answer.departmentId], function(err, res) {
+            if(err) throw err;
+            console.table(res);
+            employeeTracker();
+        });
+    });
+};
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What's the first name of the employee?",
+            name: "firstNameInput"
+        },
+        {
+            type: "input",
+            message: "What's the last name of the employee?",
+            name: "lastNameInput"
+        },
+        {
+            type: "input",
+            message: "What is the employee's role id number?",
+            name: roleId
+        },
+        {
+            type: "input",
+            message: "What is the manager id number?",
+            name: managerId
+        }
+    ]).then(function(answer) {
+
+      
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.eeFirstName, answer.eeLastName, answer.roleID, answer.managerID], function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          employeeTracker();
+        });
+});
 }
 
 employeeTracker();
